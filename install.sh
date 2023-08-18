2e18caa06fd33da9b0ede2de1fd7983a184e6a3d
Rafalb8 <rafalb8@hotmail.com>
2023-08-18 09:54:00 +0200

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+URL=$(curl -s https://api.github.com/repos/rafalb8/udns/releases/latest \
+    | grep "browser_download_url.*amd64.tar.gz" \
+    | cut -d : -f 2,3 \
+    | tr -d \")
+
+mkdir -p /tmp/udns
+curl -sfL ${URL} | tar xvz -C /tmp/udns
+sudo cp /tmp/udns/udns /bin/udns
